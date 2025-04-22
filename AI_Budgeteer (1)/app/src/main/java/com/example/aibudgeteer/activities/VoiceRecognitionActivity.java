324032e501fb735cc8556076c557dec51bf60169
cdjbm07 <cheebbeeham@gmail.com>
2025-04-22 15:42:31 -0400

    Add files via upload

---
+++ AI_Budgeteer (1)/app/src/main/java/com/example/aibudgeteer/activities/VoiceRecognitionActivity.java
@@ -0,0 +1,14 @@
+
+package com.example.aibudgeteer.activities;
+
+import android.os.Bundle;
+import android.speech.RecognizerIntent;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class VoiceRecognitionActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_voice_recognition);
+    }
+}
